8733e3fba51f4563a8499aaeeb9d8c32fe671561
csteel1 <csteel1@ithaca.edu>
2016-04-27 13:12:22 -0400

    modified build.gradle - called method causing bug added various strings and styles included new activities in manifest linked main activity to open navigator at startup for testing purposes

--- app/src/main/java/com/example/chris/safebacandroid/MainActivity.java
+++ app/src/main/java/com/example/chris/safebacandroid/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.chris.safebacandroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +13,10 @@ public class MainActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    Intent navigator = new Intent(this, Navigator_Activity.class);
+    startActivity(navigator);
+
   }
 
 
